76ffaeaf2b42742f1b40eb870be46255d1882f72
gazzahadem341-netizen <gazzahadem341@gmail.com>
2026-06-29 14:19:19 +0200

    Add files via upload

---
+++ script_ferry.py
@@ -0,0 +1,62 @@
+import re
+from playwright.sync_api import Playwright, sync_playwright, expect
+
+
+def run(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("https://www.directferries.fr/")
+    page.get_by_role("button", name="Tout accepter").click()
+    page.get_by_role("button", name="De").click()
+    page.get_by_test_id("search-widget-outbound-popular-destination-port-result-FRMRS").click()
+    page.get_by_test_id("home-search-widget-return-popup-port-field-input").click()
+    page.get_by_test_id("search-widget-return-popular-destination-port-result-TUN").click()
+    page.get_by_test_id("calendar-container-year-2026-month-1").get_by_role("button", name="11").click()
+    page.get_by_role("button", name="chevron-right").click()
+    page.get_by_test_id("calendar-container-year-2026-month-1").get_by_role("button", name="22").click()
+    page.get_by_test_id("calendar-container-year-2026-month-1").get_by_role("button", name="23").click()
+    page.get_by_test_id("calendar-container-year-2026-month-0").get_by_role("button", name="11").click()
+    page.get_by_test_id("calendar-container-year-2026-month-1").get_by_role("button", name="23").click()
+    page.get_by_test_id("search-widget-date-selector-footer-submit-button").click()
+    page.get_by_test_id("search-widget-quote-selector-popup-adult-container").get_by_test_id("counter-plus").click()
+    page.get_by_test_id("search-widget-quote-selector-popup-adult-container").get_by_test_id("counter-plus").click()
+    page.get_by_test_id("search-widget-quote-selector-vehicle-select-trigger").click()
+    page.get_by_text("Voiture").click()
+    page.get_by_test_id("search-widget-quote-selector-vehicle-select-trigger").click()
+    page.locator("html").click()
+    page.get_by_test_id("search-widget-quote-selector-vehicle-select-trigger").click()
+    page.locator("html").click()
+    page.get_by_test_id("search-widget-quote-selector-popup-confirm").click()
+    with page.expect_popup() as page1_info:
+        page.get_by_role("button", name="search Rechercher").click()
+    page1 = page1_info.value
+    page1.get_by_test_id("results-hidden-routes-toggle").click()
+    page1.get_by_test_id("results-hidden-routes-toggle").click()
+    page1.get_by_test_id("results-hidden-routes-toggle").click()
+    page1.get_by_test_id("results-next-day-button-desktop").click()
+    page1.get_by_test_id("results-hidden-routes-toggle").click()
+    page1.get_by_test_id("results-heading-shift-date-left").click()
+    page1.get_by_test_id("results-heading-shift-date-left").click()
+    page1.get_by_test_id("results-hidden-routes-toggle").click()
+    page1.get_by_test_id("results-hidden-routes-toggle").click()
+    page1.get_by_role("button", name="ven. 10 juil.").click()
+    page1.get_by_test_id("calendar-container-year-2026-month-0").get_by_role("button", name="13").click()
+    page1.get_by_test_id("calendar-container-year-2026-month-1").get_by_role("button", name="24").click()
+    page1.get_by_test_id("search-widget-date-selector-footer-submit-button").click()
+    page1.get_by_test_id("results-hidden-routes-toggle").click()
+    page1.get_by_role("button", name="lun. 13 juil.").click()
+    page1.get_by_test_id("calendar-container-year-2026-month-0").get_by_role("button", name="12").click()
+    page1.get_by_test_id("calendar-container-year-2026-month-1").get_by_role("button", name="23").click()
+    page1.get_by_test_id("search-widget-date-selector-footer-submit-button").click()
+    page1.get_by_test_id("results-hidden-routes-toggle").click()
+    page1.get_by_test_id("results-heading-shift-date-left").click()
+    page1.get_by_test_id("results-hidden-routes-toggle").click()
+
+    # ---------------------
+    context.close()
+    browser.close()
+
+
+with sync_playwright() as playwright:
+    run(playwright)
